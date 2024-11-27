31d32f9a4dbfb9542354662de23bdfea51887738
KriStiA <Valve>
2024-11-27 17:03:46 +0300

    Добавить логику обработки поступающего звонка при проигрывании аудио.

--- app/src/main/java/ru/mtuci/myapplication/MainActivity.kt
+++ app/src/main/java/ru/mtuci/myapplication/MainActivity.kt
@@ -1,9 +1,11 @@
 package ru.mtuci.myapplication
 
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
+import android.telephony.PhoneStateListener
+import android.telephony.TelephonyManager
 import android.widget.Button
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
@@ -13,12 +15,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var seekBar: SeekBar
     private val handler = Handler()
+    private lateinit var audioManager: AudioManager
+    private lateinit var telephonyManager: TelephonyManager
+    private lateinit var phoneStateListener: PhoneStateListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.missio_bottom_of_the_deep_blue_sea)
+        mediaPlayer = MediaPlayer.create(this, R.raw.lady_gaga_if_my_friends_could_see_me_now)
 
         val buttonPlayStop: Button = findViewById(R.id.ButtonPlayStop)
         seekBar = findViewById(R.id.seekBar)
@@ -28,14 +33,13 @@ class MainActivity : AppCompatActivity() {
         buttonPlayStop.setOnClickListener {
             if (!mediaPlayer.isPlaying) {
                 mediaPlayer.start()
-                buttonPlayStop.setText(getString(R.string.pause_str))
+                buttonPlayStop.setText(R.string.pause_str)
                 updateSeekBar()
             } else {
                 mediaPlayer.pause()
-                buttonPlayStop.setText(getString(R.string.play_str))
+                buttonPlayStop.setText(R.string.play_str)
             }
         }
-
 
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -44,14 +48,48 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                // Можно добавить логику, если нужно
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                // Можно добавить логику, если нужно
-            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
+
+        mediaPlayer.setOnCompletionListener {
+            buttonPlayStop.setText(R.string.play_str)
+        }
+
+        // Инициализация AudioManager и TelephonyManager для отслеживания поступающего звонка
+        audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        telephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+
+        // Создание слушателя состояния вызова
+        phoneStateListener = object : PhoneStateListener() {
+            override fun onCallStateChanged(state: Int, phoneNumber: String?) {
+
+                when (state) {
+                    TelephonyManager.CALL_STATE_RINGING -> {
+                        if (mediaPlayer.isPlaying) {
+                            mediaPlayer.pause() // Остановка воспроизведения при входящем звонке
+                            buttonPlayStop.setText(R.string.play_str)
+                        }
+                    }
+                    TelephonyManager.CALL_STATE_OFFHOOK -> {
+                        if (mediaPlayer.isPlaying) {
+                            mediaPlayer.pause() // Остановка воспроизведения при звонке с телефона
+                            buttonPlayStop.setText(R.string.play_str)
+                        }
+                    }
+                    TelephonyManager.CALL_STATE_IDLE -> {
+                        if (mediaPlayer.currentPosition != 0) {
+                            mediaPlayer.start()
+                            buttonPlayStop.setText(R.string.pause_str)
+                            updateSeekBar()
+                        }
+                    }
+                }
+            }
+        }
+
+        // Регистрация слушателя
+        telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE)
     }
 
     private fun updateSeekBar() {
@@ -64,5 +102,6 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mediaPlayer.release() // Освобождение ресурсов
+        telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE) // Отмена регистрации слушателя
     }
 }
